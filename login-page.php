6600ffab404ec7a4439c4d8965c5566dc4581d70
LittleCatXX <83004058+LittleCatXX@users.noreply.github.com>
2023-02-14 23:17:05 +0700

    Add files via upload

--- login-page.php
+++ login-page.php
@@ -7,24 +7,20 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <title>เข้าสู่ระบบ</title>
-    <a href="register.html">This is a link</a>
+    <a href="login-page.php">This is a link</a>
 </head>
 <body>
   <div class="container">
     <nav class="navbar navbar-expand-lg fixed-top" style="background-color: #fe965a;">
       <div class="container">
-        <a class="navbar-brand text-light" href="register.html">ระบบลงทะเบียนเรียน</a>
+        <a class="navbar-brand text-light" href="login-page.php">ระบบลงทะเบียนเรียน</a>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ms-auto">
           <li class="nav-item <?=$active_admin?>">
           <a class="nav-link text-light" href="course-structure.php">หลักสูตร</a>
           </li>
-          <li class="nav-item <?=$active_admin?>">
-            <a class="nav-link text-light" href=".php">วิชาที่เปิดสอน</a>
-            </li>
-          <li class="nav-item <?=$active_admin?>">
-          <a class="nav-link text-light" href="login-page.html">Log in</a>
-          </li>
+          
+          
       </ul>
       </div>
     </nav>
